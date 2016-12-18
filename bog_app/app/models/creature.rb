7494087dc1038485e813390726904c520aec0e57
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2016-12-18 11:38:26 -0800

    started fourth run

--- bog_app/app/models/creature.rb
+++
@@ -1,2 +0,0 @@
-class Creature < ApplicationRecord
-end
--- bog_app/db/seeds.rb
+++ bog_app/db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Creature.create({name: "Luke", description: "Jedi"})
-Creature.create({name: "Darth Vader", description: "Father of Luke"})
