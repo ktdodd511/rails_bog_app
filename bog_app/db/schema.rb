07505b27f1eb19ed57c67ee06027330dc1da6e7d
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2016-12-18 11:55:06 -0800

    finished fourth run, didn't commit much, oops

--- bog_app/db/schema.rb
+++ bog_app/db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218191813) do
+ActiveRecord::Schema.define(version: 20161218193915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
+  create_table "creatures", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
