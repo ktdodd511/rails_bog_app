53f84ee335cd07f64da57838bc4e63d3a5ff3a4e
ktdodd511 <katiedodd@Katies-MacBook-Air-2.local>
2016-12-18 11:07:58 -0800

    finished crud for the second time

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get "/creatures", to: "creature#index", as: "creatures"
   get "/creatures/new", to: "creature#new", as: "new_creature"
-  
+  post "/creatures", to: "creature#create"
+  get "/creatures/:id", to: "creature#show", as: "creature"
+  get "/creatures/:id/edit", to: "creature#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creature#update"
+  delete "/creatures/:id", to: "creature#destroy"
 
 end
